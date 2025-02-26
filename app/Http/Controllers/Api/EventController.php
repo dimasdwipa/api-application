5ab8388d513fdb8b33905ca2fde2b6819f51117a
Dimas Dwipa Raga <dimasdwiparaga07@gmail.com>
2025-02-26 08:19:54 +0800

    Video 10

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,38 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+
+    use CanLoadRelationships;
+
+    private array $relations = ['user','attendees','attendees.user'];
+
     public function index()
     {
-        $query = Event::query();
-        $relations = ['user','attendees','attendees.user'];
-
-        foreach ($relations as $relation){
-            $query->when(
-                $this->shouldIncludeRelation($relation),
-                fn($q) => $q -> with($relation)
-            );
-        }
+        $query = $this->loadRelationshps(Event::query());
 
         return EventResource::collection(
             $query->latest()->paginate());
-    }
-
-    protected function shouldIncludeRelation(string $relation): bool
-    {
-        $include = request()->query('include');
-
-        if (!$include){
-            return false;
-        }
-
-        $relations = array_map('trim', explode(',', $include));
-
-        return in_array($relation,$relations);
     }
 
     public function store(Request $request)
@@ -55,13 +40,12 @@ class EventController extends Controller
             'user_id' => 1,
         ]);
 
-        return new EventResource($event);
+        return new EventResource($this->loadRelationshps($event));
     }
 
     public function show(Event $event)
     {
-        $event->load('user','attendees');
-        return new EventResource($event);
+        return new EventResource($this->loadRelationshps($event));
     }
 
     public function update(Request $request, String $id)
@@ -86,7 +70,7 @@ class EventController extends Controller
             'end_time' => $validated['end_time'],
         ]);
 
-        return new EventResource($event);
+        return new EventResource($this->loadRelationshps($event));
     }
 
     public function destroy(string $id)
