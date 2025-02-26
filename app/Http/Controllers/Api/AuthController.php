e4e695a68505b45224c19340dc308d1674aa07da
Dimas Dwipa Raga <dimasdwiparaga07@gmail.com>
2025-02-26 08:57:57 +0800

    Video 12

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = \App\Models\User::where('email', $request->email)->first();
+
+        if (!$user){
+            throw ValidationException::withMessages([
+                'email' => ['The Provided Credential are Incorrect.']
+            ]);
+        }
+
+        if (!Hash::check($request->password,$user->password)){
+            throw ValidationException::withMessages([
+                'password' => ['The Provided Credential are Incorrect.']
+            ]);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
+}
